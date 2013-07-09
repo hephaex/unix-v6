328e9a3431584aef9cd023ac597e0d639278a469
MSCho <hephaex@yahoo.co.kr>
2013-07-08 21:42:06 -0700

    Update proc.h

--- proc.h
+++ proc.h
@@ -8,21 +8,21 @@
  */
 struct	proc
 {
-	char	p_stat;
-	char	p_flag;
-	char	p_pri;		/* priority, negative is high */
-	char	p_sig;		/* signal number sent to this process */
-	char	p_uid;		/* user id, used to direct tty signals */
-	char	p_time;		/* resident time for scheduling */
-	char	p_cpu;		/* cpu usage for scheduling */
-	char	p_nice;		/* nice for scheduling */
-	int	p_ttyp;		/* controlling tty */
-	int	p_pid;		/* unique process id */
-	int	p_ppid;		/* process id of parent */
-	int	p_addr;		/* address of swappable image */
-	int	p_size;		/* size of swappable image (*64 bytes) */
-	int	p_wchan;	/* event process is awaiting */
-	int	*p_textp;	/* pointer to text structure */
+	char	p_stat;		/* 상태, NULL인 경우 해당하는 PROC[]를 비어있다고 여긴다. */
+	char	p_flag;		/* 플래그 */
+	char	p_pri;		/* 실행 우선순위, 값이 작을 수록 우선순위가 높다. */
+	char	p_sig;		/* 수신된 시그날 */
+	char	p_uid;		/* 사용자 ID (정수) */
+	char	p_time;		/* 메모리나 스왑영역에 머물렀던 시간 (초) */
+	char	p_cpu;		/* CPU를 사용한 누적 시간 (tick) */
+	char	p_nice;		/* 디폴트는 0으로, nice시스템콜에 의해서 사용자가 값을 설정한다. */
+	int	p_ttyp;		/* 프로세스를 조작하고 있는 단말 */
+	int	p_pid;		/* 프로세스 ID */
+	int	p_ppid;		/* 부모 프로세스 ID */
+	int	p_addr;		/* 할당 받은 물리 메모리 어드레스 (64바이트 단위) */
+	int	p_size;		/* 할당 받은 메모리 사이즈 (64바이트 단위) */
+	int	p_wchan;	/* 슬립되고 있는 이유 */
+	int	*p_textp;	/* 사용하고 있는 텍스트세그먼트 */
 } proc[NPROC];
 
 /* stat codes */
